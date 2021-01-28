540e448667a59b4760aafd6ae50e9ae9dacbd43b
aunimhsn <aunim.hassan@gmail.com>
2021-01-28 03:45:24 +0100

    ui: landing content in progress

--- animations/js/app.js
+++ animations/js/app.js
@@ -1,11 +1,52 @@
 gsap.registerPlugin(ScrollTrigger);
 
-let tl = gsap.timeline({
+// ARTICLES
+const tlArticle = gsap.timeline({
     scrollTrigger: {
-        trigger: '.gsap-title',
-        toggleActions: 'restart none none reset'
+        trigger: '.gsap-article-title',
+        toggleActions: 'restart none none reset',
+        
     }
 });
 
-tl.from('.gsap-title', { x: -400, opacity: 0, duration: 1, ease: 'power3' })
-  .from('.gsap-subtitle', { y: -40, opacity: 0, duration: 1, ease: 'power3'}, '-=0.5');
+tlArticle.from('.gsap-article-title', { x: -400, opacity: 0, duration: 1.5, ease: 'power3' })
+         .from('.gsap-article-subtitle', { y: -40, opacity: 0, duration: 1.5, ease: 'power3'}, '-=0.5');
+
+gsap.from('.gsap-article-card', { 
+    scrollTrigger: {
+        trigger: '.gsap-article-card',
+        toggleActions: 'restart none none reset',
+    },
+    opacity: 0, 
+    duration: 1.5,
+    stagger: 0.2
+});
+
+// TODO: fix animation bug
+gsap.from('.gsap-more-article', { 
+    scrollTrigger: {
+        trigger: '.gsap-more-article',
+        toggleActions: 'play none none reset',
+        start: 'top 20%',
+        end: 'bottom 21.4%',
+        markers: true
+        
+    },
+    duration: 2,
+    rotation: 90,
+    opacity: 0.25, 
+
+    ease: 'none',
+});
+
+// CREATIONS
+const tlCreation = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.gsap-creation-title',
+        toggleActions: 'restart none none reset',
+        
+    }
+});
+
+tlCreation.from('.gsap-creation-title', { x: -400, opacity: 0, duration: 1.5, ease: 'power3' })
+         .from('.gsap-creation-subtitle', { y: -40, opacity: 0, duration: 1.5, ease: 'power3'}, '-=0.5');
