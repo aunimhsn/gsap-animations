fddd4a2cb9afadeb9d9170793730966128420b91
aunimhsn <aunim.hassan@gmail.com>
2021-01-29 19:28:17 +0100

    ui: responsive footer animated

--- animations/js/app.js
+++ animations/js/app.js
@@ -1,5 +1,31 @@
 gsap.registerPlugin(ScrollTrigger);
 
+/**
+    // https://greensock.com/docs/v3/Plugins/ScrollTrigger/static.matchMedia()
+    ScrollTrigger.matchMedia({
+        '(min-width: 800px)': () => {
+            // To not face property conflict with translate and transition, put the gsap class selector outside the box
+            // Do not need end property because we are not using scrub - Find a way to the start using velocity    
+            const tlArticleMore = gsap.timeline({
+                scrollTrigger: {
+                    trigger: '.gsap-article-more',
+                    toggleActions: 'play none none reset',
+                    markers: true
+                },
+            });
+
+
+            tlArticleMore.from('.gsap-article-more', { 
+                x: -600,
+                rotation: -90,
+                duration: 2,
+                opacity: 0, 
+                ease: 'power2',
+            });
+        }
+    });
+**/
+
 // ARTICLES
 const tlArticle = gsap.timeline({
     scrollTrigger: {
@@ -18,38 +44,38 @@ gsap.from('.gsap-article-card', {
     },
     opacity: 0, 
     duration: 1.5,
-    stagger: 0.2
+    stagger: 0.4
 });
 
-// To not face property confusion with translation and transition, put the gsap class selector outside the box
-// Do not need end property because we are not using scrub - Find a way to the start using velocity
-
-/** ANIMATE WITH MATCH MEDIA
-    
-    gsap.from('.gsap-article-more', { 
-        scrollTrigger: {
-            trigger: '.gsap-article-more',
-            toggleActions: 'play none none reset',
-            start: 'top 22%', 
-            markers: true
-        },
-        x: -600,
-        rotation: -90,
-        duration: 2,
-        opacity: 0, 
-        ease: 'power2',
-    });
-
-**/
 
 // CREATIONS
 const tlCreation = gsap.timeline({
     scrollTrigger: {
         trigger: '.gsap-creation-title',
         toggleActions: 'restart none none reset',
-        
     }
 });
 
 tlCreation.from('.gsap-creation-title', { x: -400, opacity: 0, duration: 1.5, ease: 'power3' })
-         .from('.gsap-creation-subtitle', { y: -40, opacity: 0, duration: 1.5, ease: 'power3'}, '-=0.5');
+          .from('.gsap-creation-subtitle', { y: -40, opacity: 0, duration: 1.5, ease: 'power3'}, '-=0.5');
+
+gsap.from('.gsap-creation-card', { 
+    scrollTrigger: {
+        trigger: '.gsap-creation-card',
+        toggleActions: 'restart none none reset',
+    },
+    opacity: 0, 
+    duration: 1.5,
+    stagger: 0.4
+});
+
+// FOOTER
+const tlFooter = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.gsap-footer-title',
+        toggleActions: 'restart none none reset',
+    }
+});
+
+tlFooter.from('.gsap-footer-title', { y: -200, opacity: 0, duration: 1.25 })
+        .from('.gsap-footer-verse', { x: -300, opacity: 0, duration: 1.25, ease: 'power2' });
